50bfa6934c803e0c19ed2cc7baf55df71462e0f1
I.M <morrig407@gmail.com>
2022-03-03 13:19:17 +0300

    Add solution file

---
+++ lab3Tests/RomanNumberTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class RomanNumberTests
+    {
+        [TestMethod()]
+        public void RomanNumberTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CloneTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CompareToTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
